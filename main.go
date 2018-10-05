c3f92d24a74eba1bfd29dab1d2ff81c7046ed567
Steve Huff <shuff@vecna.org>
2018-10-05 10:51:28 -0400

    refactor: use cobra and jww instead (#1)

--- main.go
+++ main.go
@@ -1,34 +1,9 @@
 package main
 
 import (
-	"os"
-
-	log "github.com/sirupsen/logrus"
-
-	"github.com/asicsdigital/dudewheresmy/commands/hostip"
-	"gopkg.in/urfave/cli.v1"
-)
-
-const (
-	appname    string = "dudewheresmy"
-	appversion string = "v0.1.0"
-	appusage   string = "find things you're looking for"
+	"github.com/asicsdigital/dudewheresmy/cmd"
 )
 
 func main() {
-	log.SetLevel(log.WarnLevel)
-
-	app := cli.NewApp()
-	app.Name = appname
-	app.Version = appversion
-	app.Usage = appusage
-
-	app.Commands = []cli.Command{
-		hostip.Command(),
-	}
-
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
-	}
+	cmd.Execute()
 }
